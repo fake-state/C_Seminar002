a061dc9ee353a16a32b9361e5c41efa3049928a3
fake_state <ntdako@gmail.com>
2023-02-13 02:46:36 +0400

    add 13

---
+++ 13/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+
+int ThirdNumber (int a)
+{
+    while (a > 999)
+    {
+        a = a / 10;
+    }
+    return a % 10;
+}
+
+Console.WriteLine("Введи трехзначное число и я найду его последнюю цифру:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 100) Console.WriteLine("Третей цифры тут нет!");
+else
+Console.WriteLine(ThirdNumber(a));
